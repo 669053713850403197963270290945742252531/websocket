f2e5f189c262900046c6f752b65f3574ef3ad412
669053713850403197963270290945742252531 <144636416+669053713850403197963270290945742252531@users.noreply.github.com>
2026-06-08 16:31:49 -0400

    Fix syntax error

--- server.js
+++ server.js
@@ -1,35 +1,44 @@
-// server.js
 const { WebSocketServer } = require("ws")
 const wss = new WebSocketServer({ port: 8080 })
 
-const clients = new Map() // userId -> ws
+const clients = new Map()
 
 wss.on("connection", (ws) => {
     let myUserId = null
 
     ws.on("message", (data) => {
-        const msg = JSON.parse(data)
+        let msg
+        try {
+            msg = JSON.parse(data)
+        } catch (e) {
+            return
+        }
 
         if (msg.type === "identify") {
             myUserId = msg.userId
-            clients.set(myUserId, ws)
+            clients.set(myUserId, { ws, displayName: msg.displayName, name: msg.name })
+
+            console.log(`[+] ${msg.displayName} (@${msg.name}) connected | userId: ${myUserId}`)
+            console.log(`[=] Total connected: ${clients.size}`)
 
             // Tell all other connected clients about this new user
-            for (const [uid, client] of clients) {
-                if (uid !== myUserId && client.readyState === 1) {
-                    client.send(JSON.stringify({
-                        type = "user_joined",
-                        userId = myUserId,
-                        displayName = msg.displayName,
-                        name = msg.name
+            for (const [uid, data] of clients) {
+                if (uid !== myUserId && data.ws.readyState === 1) {
+                    data.ws.send(JSON.stringify({
+                        type: "user_joined",
+                        userId: myUserId,
+                        displayName: msg.displayName,
+                        name: msg.name
                     }))
                 }
 
-                // Also tell the new user about everyone already connected
+                // Tell the new user about everyone already connected
                 if (uid !== myUserId) {
                     ws.send(JSON.stringify({
-                        type = "user_joined",
-                        userId = uid
+                        type: "user_joined",
+                        userId: uid,
+                        displayName: data.displayName,
+                        name: data.name
                     }))
                 }
             }
@@ -38,17 +47,26 @@ wss.on("connection", (ws) => {
 
     ws.on("close", () => {
         if (myUserId) {
+            const data = clients.get(myUserId)
+            console.log(`[-] ${data?.displayName} (@${data?.name}) disconnected`)
             clients.delete(myUserId)
 
-            // Notify others this user disconnected
-            for (const [uid, client] of clients) {
-                if (client.readyState === 1) {
-                    client.send(JSON.stringify({
-                        type = "user_left",
-                        userId = myUserId
+            for (const [uid, clientData] of clients) {
+                if (clientData.ws.readyState === 1) {
+                    clientData.ws.send(JSON.stringify({
+                        type: "user_left",
+                        userId: myUserId
                     }))
                 }
             }
+
+            console.log(`[=] Total connected: ${clients.size}`)
         }
     })
+
+    ws.on("error", (err) => {
+        console.error(`[!] Socket error: ${err.message}`)
+    })
 })
+
+console.log("[*] WebSocket server running on ws://localhost:8080")
