30f1d3d26361046e37df6ae6cb04dba84153e594
669053713850403197963270290945742252531 <144636416+669053713850403197963270290945742252531@users.noreply.github.com>
2026-06-08 16:28:59 -0400

    Update websocket server

--- server.js
+++ server.js
@@ -1,44 +1,54 @@
-const express = require("express");
-const WebSocket = require("ws");
-const http = require("http");
+// server.js
+const { WebSocketServer } = require("ws")
+const wss = new WebSocketServer({ port: 8080 })
 
-const app = express();
+const clients = new Map() // userId -> ws
 
-app.get("/", (req, res) => {
-    res.send("WebSocket server is online");
-});
-
-const server = http.createServer(app);
-
-const wss = new WebSocket.Server({
-    server
-});
-
-wss.on("connection", (ws, req) => {
-    console.log("Client connected");
-
-    ws.send(JSON.stringify({
-        type: "welcome",
-        message: "Connected to Railway WebSocket server"
-    }));
+wss.on("connection", (ws) => {
+    let myUserId = null
 
     ws.on("message", (data) => {
-        console.log("Received:", data.toString());
+        const msg = JSON.parse(data)
 
-        // Echo message back
-        ws.send(JSON.stringify({
-            type: "echo",
-            data: data.toString()
-        }));
-    });
+        if (msg.type === "identify") {
+            myUserId = msg.userId
+            clients.set(myUserId, ws)
+
+            // Tell all other connected clients about this new user
+            for (const [uid, client] of clients) {
+                if (uid !== myUserId && client.readyState === 1) {
+                    client.send(JSON.stringify({
+                        type = "user_joined",
+                        userId = myUserId,
+                        displayName = msg.displayName,
+                        name = msg.name
+                    }))
+                }
+
+                // Also tell the new user about everyone already connected
+                if (uid !== myUserId) {
+                    ws.send(JSON.stringify({
+                        type = "user_joined",
+                        userId = uid
+                    }))
+                }
+            }
+        }
+    })
 
     ws.on("close", () => {
-        console.log("Client disconnected");
-    });
-});
+        if (myUserId) {
+            clients.delete(myUserId)
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+            // Notify others this user disconnected
+            for (const [uid, client] of clients) {
+                if (client.readyState === 1) {
+                    client.send(JSON.stringify({
+                        type = "user_left",
+                        userId = myUserId
+                    }))
+                }
+            }
+        }
+    })
+})
